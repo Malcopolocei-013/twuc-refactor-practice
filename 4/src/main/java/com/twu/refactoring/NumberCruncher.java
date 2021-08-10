9cc920d2cf6880f819193d6e59d46151f4029c82
Malcopolocei-013 <2631075151@qq.com>
2021-08-11 07:29:56 +0800

    [shiwentao]: refactor countOdd by stream and extract const

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class NumberCruncher {
     public static final int EVEN = 0;
+    public static final int ODD = 1;
     private final int[] numbers;
 
 
@@ -16,11 +17,7 @@ public class NumberCruncher {
     }
 
     public int countOdd() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 1) count++;
-        }
-        return count;
+        return (int)Arrays.stream(numbers).filter(number -> number % 2 == ODD).count();
     }
 
     public int countPositive() {

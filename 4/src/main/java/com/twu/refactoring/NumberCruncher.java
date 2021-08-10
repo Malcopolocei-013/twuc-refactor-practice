b716b64c419fa93c41f7c90e9583f6d344220150
Malcopolocei-013 <2631075151@qq.com>
2021-08-11 07:24:34 +0800

    [shiwentao]: refactor countEven by stream and extract const

--- 4/src/main/java/com/twu/refactoring/NumberCruncher.java
+++ 4/src/main/java/com/twu/refactoring/NumberCruncher.java
@@ -1,18 +1,18 @@
 package com.twu.refactoring;
 
+import java.util.Arrays;
+
 public class NumberCruncher {
+    public static final int EVEN = 0;
     private final int[] numbers;
+
 
     public NumberCruncher(int... numbers) {
         this.numbers = numbers;
     }
 
     public int countEven() {
-        int count = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) count++;
-        }
-        return count;
+        return (int)Arrays.stream(numbers).filter(number -> number % 2 == EVEN).count();
     }
 
     public int countOdd() {

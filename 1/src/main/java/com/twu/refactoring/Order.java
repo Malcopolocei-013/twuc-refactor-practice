48ef1f88c34743b564f9e93baf7a51e5dac4b298
Malcopolocei-013 <2631075151@qq.com>
2021-08-11 07:38:48 +0800

    [shiwentao]: refactor name in Order

--- 1/src/main/java/com/twu/refactoring/Order.java
+++ 1/src/main/java/com/twu/refactoring/Order.java
@@ -3,25 +3,25 @@ package com.twu.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    String name;
+    String address;
+    List<LineItem> lineItems;
 
     public Order(String nm, String addr, List<LineItem> li) {
-        this.nm = nm;
-        this.addr = addr;
-        this.li = li;
+        this.name = nm;
+        this.address = addr;
+        this.lineItems = li;
     }
 
     public String getCustomerName() {
-        return nm;
+        return name;
     }
 
     public String getCustomerAddress() {
-        return addr;
+        return address;
     }
 
     public List<LineItem> getLineItems() {
-        return li;
+        return lineItems;
     }
 }

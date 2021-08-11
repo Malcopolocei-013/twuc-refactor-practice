506dfbb63420d3e38ebfa4dd949640f9e9684341
Malcopolocei-013 <2631075151@qq.com>
2021-08-11 23:35:28 +0800

    [shiwentao]: refactor turn and merge the equals

--- 3/src/main/java/com/twu/refactoring/Direction.java
+++ 3/src/main/java/com/twu/refactoring/Direction.java
@@ -3,15 +3,14 @@ package com.twu.refactoring;
 public class Direction {
     private final char direction;
     private static final String DIRECTIONS = "NWSE";
+
     public Direction(char direction) {
         this.direction = direction;
     }
 
-    public Direction turnRight() {
-        return new Direction(DIRECTIONS.charAt((DIRECTIONS.indexOf(direction) + 3) % 4));
-    }
-
-    public Direction turnLeft() {
+    public Direction turn(String leftOrRight) {
+        if (leftOrRight.equals("right"))
+            return new Direction(DIRECTIONS.charAt((DIRECTIONS.indexOf(direction) + 3) % 4));
         return new Direction(DIRECTIONS.charAt((DIRECTIONS.indexOf(direction) + 1) % 4));
     }
 
@@ -20,7 +19,6 @@ public class Direction {
         Direction directionObj = (Direction) obj;
 
         if (direction != directionObj.direction || obj == null || getClass() != obj.getClass()) return false;
-
         return true;
     }
 }
